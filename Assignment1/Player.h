17268ff2c9c9c9a2695063f089e198f7372164f8
Fernatha <fernatha@sheridancollege.ca>
2019-10-24 16:22:58 -0400

    blep blip

---
+++ Assignment1/Player.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+class Player {
+private:
+	const float START_HEALTH = 4;
+	//where is the player
+	Vector3f m_Position;
+
+	Sprite m_Sprite;
+
+	Texture m_Texture;
+
+	Vector2f m_Resolution;
+
+	IntRect m_Arena;
+
+	int m_TileSize;
+
+	bool leftPressed;
+	bool rightPressed;
+	bool jumpPressed;
+
+	int m_Health;
+
+	Time m_LastHit;
+
+	float m_Speed = 200;
+
+public:
+	Player();
+	void spawn(IntRect arena, Vector2f resolution, int tilesize);
+
+	void resetPlayerStats();
+
+	bool hit(Time timeHit);
+
+	Time getLastHitTime();
+
+	FloatRect getPosition();
+};
